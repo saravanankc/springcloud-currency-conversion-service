9c04a7e4fff28ca8ba544b9b86aae217500daa4f
saravanannachiappan <snachiappan@solstice.com>
2020-03-19 12:47:02 -0500

    Registered service to Eurekha server

--- src/main/java/com/kc/mylearnings/springcloud/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/kc/mylearnings/springcloud/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,9 +2,11 @@ package com.kc.mylearnings.springcloud.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients("com.kc.mylearnings.springcloud.microservices.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
